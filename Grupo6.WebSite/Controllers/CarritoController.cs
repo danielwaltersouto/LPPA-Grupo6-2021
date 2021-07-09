b403118f96673207f90be89a7ea5d07bb9988456
Ricardo Misme <misme.ricardo@gmail.com>
2021-07-09 17:26:48 -0300

    abm carrito

---
+++ Grupo6.WebSite/Controllers/CarritoController.cs
@@ -0,0 +1,16 @@
+﻿using Grupo6.Business;
+using System.Web.Mvc;
+
+namespace Grupo6.WebSite.Controllers
+{
+    public class CarritoController : Controller
+    {
+        // GET: Carrito
+        public ActionResult Index()
+        {
+            var bizCarrito = new BizCarrito();
+            var model = bizCarrito.TraerTodos();
+            return View(model);
+        }
+    }
+}
