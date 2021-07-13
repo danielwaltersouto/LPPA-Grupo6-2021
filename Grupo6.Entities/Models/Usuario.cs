713515b5dd7f28b3646c9e12c8ad7a5fd64f69bb
danielwaltersouto <82915729+danielwaltersouto@users.noreply.github.com>
2021-07-13 20:04:53 -0300

    Revert "Data annotation"

--- Grupo6.Entities/Models/Usuario.cs
+++ Grupo6.Entities/Models/Usuario.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Grupo6.Entities.Models
@@ -13,72 +12,20 @@ namespace Grupo6.Entities.Models
             Bloqueo = 0;
             EmailConfirmed = false;
         }
-
-        [Display(Name = "Categoria Fiscal")]
-        public int IdCategoriaFiscal { get; set; }
-       
-        [Display(Name = "Usuario WEB")]
-
         public DateTime FechaAlta { get; set; }
         public int CategoriaFiscalId { get; set; }
-
         public string NombreWeb { get; set; }
-      
-        [Required(ErrorMessage = "Se requiere Password")]
-        [DataType(DataType.Password)]
         public string Password { get; set; }
-
-
-        [NotMapped]
-        [Compare(nameof(Email), ErrorMessage = "Password no Coincide")]
-        [Required(ErrorMessage = "Se requiere Password")]
-        [Display(Name = "Re Ingrese Password")]
-        [DataType(DataType.Password)]
-
-        public string PasswordCheck { get; set; }
-
-        public string Token { get; set; }
-        public int Bloqueo { get; set; }
-        public int IntentosLogin { get; set; }
-        public int IdRol { get; set; }
-        
-        
-        [Required(ErrorMessage = "Se requiere Nombre")]
-        [MaxLength(30)]
-
         public int Bloqueo { get; set; }
         public int RolId { get; set; }
-
         public string Nombre { get; set; }
-        
-        
-        [Required(ErrorMessage = "Se requiere Apellido")]
-        [MaxLength(30)]
         public string Apellido { get; set; }
-       
-        [Required(ErrorMessage = "Se requiere Documento")]
-        [StringLength(8, ErrorMessage = "Requiere 8 numeros")]
         public int Documento { get; set; }
-
-        [Required(ErrorMessage ="Se requiere fecha" )]
         public DateTime FechaNacimiento { get; set; }
-        public DateTime FechaAlta { get; set; }
-
         public int Telefono { get; set; }
-        
-        [EmailAddress]
-        [Required(ErrorMessage = "Se requiere Email")]
         public string Email { get; set; }
-        
-        [NotMapped]
-        [EmailAddress]
-        [Required(ErrorMessage = "Se requiere Email")]
-        [Display(Name = "Re-Ingreso Email")]
-        [Compare(nameof(Email), ErrorMessage = "Email no Coincide")]
         public bool EmailConfirmed { get; set; }
         public string UserToken { get; set; }
-
-
 
         public virtual ICollection<Carrito> Carrito { get; set; }
         public virtual CategoriaFiscal CategoriaFiscal { get; set; }
