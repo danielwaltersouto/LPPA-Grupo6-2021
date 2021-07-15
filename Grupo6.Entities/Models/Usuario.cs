97df7321d9cf048fde0fc1195e8d68cdfb55dfde
Ricardo Misme <misme.ricardo@gmail.com>
2021-07-15 14:21:27 -0300

    fix errores de mergeo

--- Grupo6.Entities/Models/Usuario.cs
+++ Grupo6.Entities/Models/Usuario.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Grupo6.Entities.Models
@@ -14,46 +15,37 @@ namespace Grupo6.Entities.Models
         }
 
         [Display(Name = "Categoria Fiscal")]
-        public int IdCategoriaFiscal { get; set; }
-       
-        [Display(Name = "Usuario WEB")]
-
-   
         public int CategoriaFiscalId { get; set; }
+
+        [Display(Name = "Usuario WEB")]
         public string NombreWeb { get; set; }
         public string Password { get; set; }
         public int Bloqueo { get; set; }
         public int IntentosLogin { get; set; }
-        public int IdRol { get; set; }
-        
-        
+
         [Required(ErrorMessage = "Se requiere Nombre")]
         [MaxLength(30)]
         public string Nombre { get; set; }
 
-    
-        public int RolId { get; set; }
-
-     
         [Required(ErrorMessage = "Se requiere Apellido")]
         [MaxLength(30)]
         public string Apellido { get; set; }
         public int Documento { get; set; }
         public DateTime FechaNacimiento { get; set; }
-       
+
         public DateTime FechaAlta { get; set; }
 
         public int Telefono { get; set; }
         public string Email { get; set; }
-        
+
         [NotMapped]
         [EmailAddress]
         [Required(ErrorMessage = "Se requiere Email")]
         [Display(Name = "Re-Ingreso Email")]
         [Compare(nameof(Email), ErrorMessage = "Email no Coincide")]
-       
         public bool EmailConfirmed { get; set; }
         public string UserToken { get; set; }
+        public int RolId { get; set; }
 
         public virtual ICollection<Carrito> Carrito { get; set; }
         public virtual CategoriaFiscal CategoriaFiscal { get; set; }
