8098d61cf644294cc5e00bb1b039aac385e7ae89
Ricardo Misme <misme.ricardo@gmail.com>
2021-07-16 01:32:42 -0300

    add cart view

---
+++ Grupo6.WebSite/Controllers/CarritoController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Grupo6.WebSite.Controllers
+{
+    public class CarritoController : Controller
+    {
+        // GET: Carrito
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        [HttpGet]
+        public ActionResult MyCart()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public ActionResult AddItemCart()
+        {
+            return View();
+        }
+    }
+}
