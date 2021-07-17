d9621e1c9791a33525d08bf319f5a77e03ede92c
Daniel <daniel.walter.souto@gmail.com>
2021-07-16 23:45:43 -0300

    fix using

--- Grupo6.Entities/Models/Usuario.cs
+++ Grupo6.Entities/Models/Usuario.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.ComponentModel.DataAnnotations;
+
 
 namespace Grupo6.Entities.Models
 {
