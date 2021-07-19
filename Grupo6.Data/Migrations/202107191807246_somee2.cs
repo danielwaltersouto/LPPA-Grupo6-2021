959093a4140fa32a8f794b6afcba29ffb0e5136a
Daniel <daniel.walter.souto@gmail.com>
2021-07-19 15:11:49 -0300

    somee2

---
+++ Grupo6.Data/Migrations/202107191807246_somee2.cs
@@ -0,0 +1,16 @@
+﻿namespace Grupo6.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class somee2 : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
